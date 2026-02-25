5d36f3d258d9007e0ad52fae024902133d890392
eduaserr <157403329+eduaserr@users.noreply.github.com>
2026-02-25 18:26:10 +0100

    comments removed

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 03:22:02 by eduaserr          #+#    #+#             */
-/*   Updated: 2026/02/17 19:25:09 by eduaserr         ###   ########.fr       */
+/*   Updated: 2026/02/25 18:25:32 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,45 +52,3 @@ int	main(int ac, char **av)
 	free_all(&game);
 	return (0);
 }
-
-/*
-TODO
-# 1. Mapa sin jugador
-# 2. Múltiples jugadores
-# 3. Texturas faltantes
-# 4. Colores inválidos
-# 5. Mapa no cerrado
-# 6. Caracteres inválidos
-# 7. Líneas vacías dentro del mapa
-
-PARSER
-comprobar que el path de la textura está completo. (solved, creo. need probar mas casos)
-leak en parsergb si le pasas un rgb incompleto (solved)
-seguir con comprobacion demapa.
-bordes, entidades... (check)
-
-Debo de mantener las lienas vacias dentro del mapa (culpa de gnl -> split) se salta las lineas vacias "" (solved)
-
-En el chequeo guardar valores necesarios para facilitar la ejecucion
-si hay 0, comprueba las paredes alrededor.
-
-
-   1     111111111111111111111111		(mapa valido)
-        1000000000110000000000001
-        1111111111111111111111111
-
-¿Que sucede si hay una pared suelta fuera del mapa?
-valido
-
-SIEMPRE comprobar mallocs y memoria sin liberar.
-Liberar en todas las salidas de error.
-
-ft_error(char *str) muestra el mensaje(str) deseado y hace exit() dentro de la funcion,
-por lo que se debe liberar todo lo necesario justo antes de llamar a esta funcion.
-NO EDITAR ft_error().
-ft_error2() simplemente muestra un mensaje de error pero no sale del programa
-
-cambiar disposicion de los mensajes de errores, debe ser Error\n.
-
-Liberar al final de la ejecucion. Gestión de ventanas y mlx.
-*/
