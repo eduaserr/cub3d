b0b29817edf4fde44f84c739a32d60c084953bdd
eduaserr <eduaserr@student.42malaga.com>
2026-02-28 21:49:19 +0100

    norminette

--- src/parse/map_validation.c
+++ src/parse/map_validation.c
@@ -6,7 +6,7 @@
 /*   By: eduaserr <eduaserr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 03:22:02 by eduaserr          #+#    #+#             */
-/*   Updated: 2026/02/28 20:57:32 by eduaserr         ###   ########.fr       */
+/*   Updated: 2026/02/28 21:39:43 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,13 +60,13 @@ int	is_map_line(char *line)
 
 int	find_end(char **file)
 {
-    int	i;
+	int	i;
 
-    i = 0;
-    while (file[i])
-        i++;
-    i--;
-    while (i >= 0 && is_empty_line(file[i]))
-        i--;
-    return (i);
+	i = 0;
+	while (file[i])
+		i++;
+	i--;
+	while (i >= 0 && is_empty_line(file[i]))
+		i--;
+	return (i);
 }
